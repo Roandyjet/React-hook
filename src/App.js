7e8612c27f51490553166399e2313dbed2343e5c
Rova <roandyjet@gmail.com>
2022-11-14 10:49:25 +0300

    useState training

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+// ______________________Increase Decrease number________________
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  // ________________________show current time___________________
+  let now = new Date().toLocaleTimeString();
+
+  const [time, setTime] = useState(now);
+
+  function showTime() {
+    let now = new Date().toLocaleTimeString();
+    setTime(now);
+  }
+
+  setInterval(showTime, 1000);
+
+  // ____________________________________________________________
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{count}</h1>
+      <button className="count" onClick={increase}>
+        +
+      </button>
+      <button className="count" onClick={decrease}>
+        -
+      </button>
+      <div className="container">
+        <h2>{time}</h2>
+        <button className="getTime" onClick={showTime}>
+          Show time
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
